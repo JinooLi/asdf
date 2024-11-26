1d06697dc995d6d33bba679c5f1c5c4c858aa51c
wontothree <prepared7913@naver.com>
2024-11-26 21:36:58 +0900

    맵에 따른 궤적 계산 명령어 shell script로 추가

--- scripts/set_alias.sh
+++ scripts/set_alias.sh
@@ -2,11 +2,14 @@
 
 path=$(dirname $BASH_SOURCE)
 
-# launch_base_system <map name>
+# usage: launch_base_system <map name>
 alias launch_base_system=". ${path}/base_system.sh"
 
-# launch_mppi
+# usage: launch_mppi
 alias launch_mppi=". ${path}/mppi.sh"
 
-# launch_slam <map name>
+# usage: launch_slam <map name>
 alias launch_slam=". ${path}/slam.sh"
+
+# usage: launch_recalc_trj <map name>
+alias launch_recalc_trj=". ${path}/recalc_trj.sh"
