b83b84de1790d4fcec8e5e909c3fdce2e49e9104
wontothree <prepared7913@naver.com>
2024-11-26 19:55:21 +0900

    Feat: launch 명령어 한번에 실행할 수 있도록 하는 bash script 작성

---
+++ src/scripts/set_alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+path="/home/turtleship/turtleship_ws/src/scripts"
+
+alias launch_base_system=". ${path}/base_system.sh"
+
+alias launch_mppi=". ${path}/mppi.sh"
+
+alias launch_slam=". ${path}/slam.sh"
---
+++ src/scripts/slam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+roslaunch stack_master mapping.launch map_name:=mppi_v2 racecar_version:=NUC2
