2a0d216bf5a9b9977b1dfd48c896098be3a7d2fd
JinooLi <jinu1423@naver.com>
2024-12-12 21:53:11 +0900

    fix: 내부의 명령어가 오류를 내면 거기에서 실행을 중단하도록 편집

--- scripts/set_global_csv.sh
+++ scripts/set_global_csv.sh
@@ -6,16 +6,21 @@ if [ -z "$MAP_NAME" ]; then
     exit 1
 fi
 
+echo "MAP_NAME: $MAP_NAME"
+
+current_path=$(pwd)
+
 workspace_path=/home/turtleship/turtleship_ws
 
+# global_waypoints.csv 복사
 cp $workspace_path/src/race_stack/stack_master/maps/$MAP_NAME/global_waypoints.csv \
-    $workspace_path/src/proj-svg_mppi/src/reference_waypoint_loader/data
-
-cd $workspace_path/src/proj-svg_mppi/src/reference_waypoint_loader/data
-
-rm reference_waypoints.csv
-
+    $workspace_path/src/proj-svg_mppi/src/reference_waypoint_loader/data && \
+# 이동
+cd $workspace_path/src/proj-svg_mppi/src/reference_waypoint_loader/data && \
+# 기존 reference_waypoints.csv 삭제
+rm reference_waypoints.csv && \
 # 파일 이름 변경
-mv global_waypoints.csv reference_waypoints.csv
-
+mv global_waypoints.csv reference_waypoints.csv && \
 echo "global_waypoints.csv is set"
+
+cd $current_path
